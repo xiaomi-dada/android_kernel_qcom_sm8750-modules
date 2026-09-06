e720ba0b2f729da8e47fc7bbb239d3289755bbc4
Iscle <albertiscle9@gmail.com>
2026-09-06 22:09:35 +0200

    touch-drivers: Build the Synaptics TCM2 driver
    The panel's touch controller, on QTI Touch Services.

--- qcom/opensource/touch-drivers/synaptics_tcm2/syna_tcm2_runtime.h
+++ qcom/opensource/touch-drivers/synaptics_tcm2/syna_tcm2_runtime.h
@@ -145,15 +145,19 @@ extern struct device *syna_request_managed_device(void);
 /*
  * Abstractions of data comparison
  */
+#ifndef MAX
 #define MAX(a, b) \
 	({__typeof__(a) _a = (a); \
 	__typeof__(b) _b = (b); \
 	_a > _b ? _a : _b; })
+#endif
 
+#ifndef MIN
 #define MIN(a, b) \
 	({__typeof__(a) _a = (a); \
 	__typeof__(b) _b = (b); \
 	_a < _b ? _a : _b; })
+#endif
 #endif
 
 /*
